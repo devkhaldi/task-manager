ad53d26defda76f36a3e0b0fc35b75de8d0b6e78
devkhaldi <dev.khaldi@gmail.com>
2021-02-23 22:58:52 +0100

    Init style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div>
-      <h2>Welcome to task manager</h2>
-    </div>
-  )
+    return (
+        <div>
+            <h2>Welcome to task manager</h2>
+        </div>
+    )
 }
 
 export default App
