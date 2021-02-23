9f5442852901d0f4f369df43cdfcae7ba0700bce
devkhaldi <dev.khaldi@gmail.com>
2021-02-23 23:23:48 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Header from './components/Header'
 function App() {
     return (
-        <div>
-            <h2>Welcome to task manager</h2>
+        <div className='container'>
+            <Header />
         </div>
     )
 }
