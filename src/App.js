88b665b3ddb45d7d50c1b112093638a68fa942cb
devkhaldi <dev.khaldi@gmail.com>
2021-03-01 23:29:14 +0100

    update task

--- src/App.js
+++ src/App.js
@@ -5,48 +5,54 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
-    const [tasks, setTasks] = useState([])
+  const url = 'http://localhost:5000/tasks'
+  const [tasks, setTasks] = useState([])
 
-    // Add task form state
-    const [formTask, setFormTask] = useState({ title: '', date: '', reminder: false })
+  // Add task form state
+  const [formTask, setFormTask] = useState({ title: '', date: '', reminder: false })
 
-    useEffect(() => {
-        axios
-            .get('http://localhost:5000/tasks')
-            .then(res => setTasks(res.data))
-            .catch(err => console.log(err))
-    }, [])
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(res => setTasks(res.data))
+      .catch(err => console.log(err))
+  }, [])
 
-    const onDelete = id => {
-        if (window.confirm('Delete task')) {
-            axios
-                .delete(`http://localhost:5000/tasks/${id}`)
-                .then(res => console.log(res))
-                .catch(err => console.log(err))
-
-            setTasks(tasks.filter(task => task.id !== id))
-        }
+  const onDelete = id => {
+    if (window.confirm('Delete task')) {
+      axios
+        .delete(`${url}/${id}`)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+      setTasks(tasks.filter(task => task.id !== id))
     }
-    const onDoubleClick = id => {
-        setTasks(tasks.map(task => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
+  }
+  const onDoubleClick = id => {
+    const task = tasks.find(task => task.id === id)
+    const updatedTask = { ...task, reminder: !task.reminder }
+    axios
+      .put(`${url}/${id}`, updatedTask)
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+    setTasks(tasks.map(task => (task.id === id ? { ...task, reminder: !task.reminder } : task)))
+  }
+  const addTask = () => {
+    if (formTask.title && formTask.date) {
+      axios
+        .post(url, formTask)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+      setFormTask({ ...formTask, id: formTask.id + 1, title: '', date: '' })
     }
-    const addTask = () => {
-        if (formTask.title && formTask.date) {
-            axios
-                .post('http://localhost:5000/tasks', formTask)
-                .then(res => console.log(res))
-                .catch(err => console.log(err))
-            setFormTask({ ...formTask, id: formTask.id + 1, title: '', date: '' })
-        }
-    }
+  }
 
-    return (
-        <div className='container'>
-            <Header addTask={addTask} />
-            <AddTask formTask={formTask} setFormTask={setFormTask} />
-            <Tasks tasks={tasks} onDelete={onDelete} onDoubleClick={onDoubleClick} />
-        </div>
-    )
+  return (
+    <div className='container'>
+      <Header addTask={addTask} />
+      <AddTask formTask={formTask} setFormTask={setFormTask} />
+      <Tasks tasks={tasks} onDelete={onDelete} onDoubleClick={onDoubleClick} />
+    </div>
+  )
 }
 
 export default App
